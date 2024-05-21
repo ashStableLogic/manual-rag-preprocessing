8a321c9d6421d0e87d83050f53a674dc23c45e32
Ashley Gittens <ash.gittens@stablelogic.com>
2024-05-21 12:11:59 +0100

    added image annotations

--- main.py
+++ main.py
@@ -3,36 +3,54 @@ import os
 import pymupdf
 from argparse import ArgumentParser
 
-SEP = os.pathsep
+SEP = "\\"
 
 
-def process_pdf_images(manual_path, output_folder, output_image_folder):
+def process_pdf_images(
+    manual_path,
+    output_file_path,
+    output_folder_name,
+    output_folder_path,
+    output_image_folder_name,
+    output_image_folder_path,
+):
 
     doc = pymupdf.open(manual_path)
 
     for page_index, page in enumerate(doc):
 
-        image_refs = page.get_images()
+        page.clean_contents()
+
+        page_name = f"page_{page_index+1}"
+
+        image_refs = page.get_images(full=True)
 
         if image_refs:
-            print(f"{len(image_refs)} found on page {page_index+1}")
+            print(f"{len(image_refs)} image(s) found on page {page_index+1}")
         else:
             print(f"No images on page {page_index+1}")
 
         for image_index, image_ref in enumerate(image_refs):
 
-            image_name = f"page_{page_index+1}_image_{image_index+1}"
+            image_name = page_name + f"_image_{image_index+1}"
 
-            image_path = output_image_folder + SEP + image_name
+            image_file_name = output_image_folder_name + SEP + image_name
+            image_file_path = os.path.abspath(image_file_name)
 
             xref = image_ref[0]
             pix = pymupdf.Pixmap(doc, xref)
-            bbox = pymupdf.Page.get_image_bbox(page, xref)
+            bbox = page.get_image_bbox(image_ref)
 
             if pix.n - pix.alpha > 3:  # CMYK: convert to RGB first
                 pix = pymupdf.Pixmap(pymupdf.csRGB, pix)
 
-            pix.save(image_path)
+            pix.save(f"{image_file_path}.png")
+
+            page.add_redact_annot(bbox, text=image_file_name)
+
+            page.apply_redactions()
+
+    doc.save(output_file_path)
 
 
 def main(args):
@@ -48,15 +66,31 @@ def main(args):
     assert manual_filenames, "no manuals to process"
 
     for manual_filename in manual_filenames:
+        manual_name = manual_filename.split(".")[0]
         manual_path = os.path.abspath(f"./manuals/{manual_filename}")
-        output_folder = f"output" + SEP + "{manual_filename}"
-        output_images_folder = f"output" + SEP + {manual_filename} + SEP + "images"
 
-        if not os.path.exists(os.path.abspath(output_folder)) or redo:
-            os.makedirs(os.path.abspath("./output/"), exist_ok=True)
-            os.makedirs(os.path.abspath("./output/images"), exist_ok=True)
+        output_folder_name = "output" + SEP + manual_name
+        output_file_name = "output" + SEP + manual_name + SEP + manual_filename
+        output_images_folder_name = f"output" + SEP + manual_name + SEP + "images"
 
-            process_pdf_images(manual_path, output_folder, output_images_folder)
+        output_folder_path = os.path.abspath(output_folder_name)
+        output_file_path = os.path.abspath(output_file_name)
+        output_images_folder_path = os.path.abspath(output_images_folder_name)
+
+        if not os.path.exists(os.path.abspath(output_folder_name)) or redo:
+            print(f"processing {manual_name}")
+
+            os.makedirs(output_folder_path, exist_ok=True)
+            os.makedirs(output_images_folder_path, exist_ok=True)
+
+            process_pdf_images(
+                manual_path,
+                output_file_path,
+                output_folder_name,
+                output_folder_path,
+                output_images_folder_name,
+                output_images_folder_path,
+            )
 
 
 if __name__ == "__main__":
@@ -66,7 +100,11 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--manuals-folder", dest="manuals_folder", required=True)
 
     parser.add_argument(
-        "-r", "--redo-processed-manuals", dest="redo_processed_manuals", default=False
+        "-r",
+        "--redo-processed-manuals",
+        dest="redo_processed_manuals",
+        type=bool,
+        default=False,
     )
 
     args = parser.parse_args()

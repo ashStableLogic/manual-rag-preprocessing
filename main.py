49e66f6e76c266f28ce6599448b993d75b71c879
Ash Gittens <ash.gittens@stablelogic.com>
2024-05-22 14:35:42 +0100

    Added pymupdf native drawing grouping and extraction

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ MIN_IMAGE_WIDTH = 30
 ZOOM_X, ZOOM_Y = 2.0, 2.0  # This stops image resolution from being quartered
 ZOOM_MAT = pymupdf.Matrix(ZOOM_X, ZOOM_Y)
 
+FIGURE_X_GROUPING_ADJACENCY_PX = 3
+FIGURE_Y_GROUPING_ADJACENCY_PX = 3
+
 ###DETECTRON2 CONSTS
 DETECTRON_CONFIG = "lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config"
 DETECTRON_EXTRA_CONFIG = [
@@ -109,7 +112,44 @@ def process_pdf_images(page, page_index, relative_image_folder_path):
     return page
 
 
-def process_pdf_figures(page, page_index, relative_figure_folder_path, layout_model):
+def process_pdf_figures(page, page_index, relative_figure_folder_path):
+
+    page_name = f"page {page_index+1}"
+
+    figures = page.cluster_drawings()
+
+    if figures:
+        print(f"{len(figures)} figures(s) found on page {page_index+1}")
+
+        relative_page_path = os.path.join(relative_figure_folder_path, page_name)
+        absolute_page_path = os.path.abspath(relative_page_path)
+
+        os.makedirs(absolute_page_path, exist_ok=True)
+    else:
+        print(f"No figures on page {page_index+1}")
+        return page
+
+    for figure_index, figure in enumerate(figures):
+
+        figure_name = f"image {figure_index+1}.png"
+
+        relative_figure_file_path = os.path.join(
+            relative_figure_folder_path, page_name, figure_name
+        )
+        absolute_figure_file_path = os.path.abspath(relative_figure_file_path)
+
+        figure_pixmap = page.get_pixmap(matrix=ZOOM_MAT, clip=figure)
+
+        figure_pixmap.pil_save(absolute_figure_file_path)
+
+        page.add_redact_annot(figure, text=relative_figure_file_path)
+
+    return page
+
+
+def process_pdf_figures_with_detectron(
+    page, page_index, relative_figure_folder_path, layout_model
+):
     """Gets around not being able to convert a
     drawing to an image directly by copying the image
     into a temporary document and creating a Pixmap from that
@@ -199,7 +239,7 @@ def process_pdf_pages(
 
     relative_image_folder_path = os.path.join(relative_output_folder_path, "images")
 
-    relative_drawing_folder_path = os.path.join(relative_output_folder_path, "figures")
+    relative_figure_folder_path = os.path.join(relative_output_folder_path, "figures")
 
     write_doc = pymupdf.open(manual_path)
 
@@ -216,9 +256,11 @@ def process_pdf_pages(
         # (if you have a cuda-enabled gpu handy and have built detectron2
         # with it in mind, remove the MODEL.DEVICE and cpu strings from
         # the extra detectron config const)
-        page = process_pdf_figures(
-            page, page_index, relative_drawing_folder_path, layout_model
-        )
+        # page = process_pdf_figures_with_detectron(
+        #     page, page_index, relative_figure_folder_path, layout_model
+        # )
+
+        page = process_pdf_figures(page, page_index, relative_figure_folder_path)
 
         print()
 
